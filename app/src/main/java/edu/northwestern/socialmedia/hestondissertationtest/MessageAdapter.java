a7e40950fd6e9d99cccca328dd65d22c1ec07c09
Matthew Heston <matthewheston@gmail.com>
2018-01-29 13:27:47 -0600

    Start to make listview nicer.

---
+++ app/src/main/java/edu/northwestern/socialmedia/hestondissertationtest/MessageAdapter.java
@@ -0,0 +1,61 @@
+package edu.northwestern.socialmedia.hestondissertationtest;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.text.Format;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+
+/**
+ * Created by matthewheston on 1/29/18.
+ */
+
+public class MessageAdapter extends BaseAdapter {
+    private Context mContext;
+    private LayoutInflater mInflater;
+    private ArrayList<Message> mDataSource;
+
+    public MessageAdapter(Context context, ArrayList<Message> items) {
+        mContext = context;
+        mDataSource = items;
+        mInflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public int getCount() {
+        return mDataSource.size();
+    }
+
+    //2
+    @Override
+    public Object getItem(int position) {
+        return mDataSource.get(position);
+    }
+
+    //3
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    //4
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // Get view for row item
+        View rowView = mInflater.inflate(R.layout.list_item_message, parent, false);
+
+        TextView messageHolder = rowView.findViewById(R.id.list_message_holder);
+        TextView dateHolder = rowView.findViewById(R.id.date_holder);
+
+        Message msg = (Message) getItem(position);
+        String msgText = msg.getMessageText();
+        messageHolder.setText(msgText);
+
+        return rowView;
+    }
+}
--- app/src/main/java/edu/northwestern/socialmedia/hestondissertationtest/MessageListActivity.java
+++ app/src/main/java/edu/northwestern/socialmedia/hestondissertationtest/MessageListActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MessageListActivity extends AppCompatActivity {
@@ -21,14 +22,7 @@ public class MessageListActivity extends AppCompatActivity {
         ListView mListView = findViewById(R.id.messageList);
         List<Message> messages = db.messageDao().getAll();
 
-        String[] listItems = new String[messages.size()];
-
-        for(int i = 0; i < messages.size(); i++){
-            Message msg = messages.get(i);
-            listItems[i] = msg.getMessageFrom();
-        }
-
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, listItems);
+       MessageAdapter adapter = new MessageAdapter(this, new ArrayList<Message>(messages));
         mListView.setAdapter(adapter);
     }
 }
