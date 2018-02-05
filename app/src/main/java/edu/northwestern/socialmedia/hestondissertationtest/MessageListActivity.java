5179ad670f4eb4436a78270c5f5bf0bea53b1229
Matthew Heston <matthewheston@gmail.com>
2018-02-05 17:01:45 -0600

    Make list view clickable.

--- app/src/main/java/edu/northwestern/socialmedia/hestondissertationtest/MessageListActivity.java
+++ app/src/main/java/edu/northwestern/socialmedia/hestondissertationtest/MessageListActivity.java
@@ -1,9 +1,13 @@
 package edu.northwestern.socialmedia.hestondissertationtest;
 
 import android.arch.persistence.room.Room;
+import android.content.Context;
+import android.content.Intent;
 import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -20,9 +24,29 @@ public class MessageListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_message_list);
 
         ListView mListView = findViewById(R.id.messageList);
-        List<Message> messages = db.messageDao().getUnhandled();
+        final List<Message> messages = db.messageDao().getUnhandled();
 
        MessageAdapter adapter = new MessageAdapter(this, new ArrayList<Message>(messages));
         mListView.setAdapter(adapter);
+
+        final Context context = this;
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // 1
+                Message selectedMessage = messages.get(position);
+
+                // 2
+                Intent rateIntent = new Intent(context, MainActivity.class);
+
+                // 3
+                rateIntent.putExtra("message_id", (long) selectedMessage.getUid());
+
+                // 4
+                startActivity(rateIntent);
+            }
+
+        });
     }
 }
